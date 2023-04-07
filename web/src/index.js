2cbde845a8ea41876db4991eafabd3c863d98a68
gnunez <generoso.nunez@gmail.com>
2023-04-07 17:00:58 -0400

    web/js files to call the api

---
+++ web/src/index.js
@@ -0,0 +1,15 @@
+(async ($) => {
+  console.log("hello world")
+  const response = await fetch("/status");
+  const jsonData = await response.json();
+  console.log(jsonData)
+  const echoResponse = await fetch("/echo",
+    {
+      method: "POST",
+      body: JSON.stringify({ message: "hello" }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  );
+})()
